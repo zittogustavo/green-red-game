b6b48f5937531a8116b4b32acd316f529e1c5f1a
gzitto <gzitto@easyvirtualfair.com>
2021-10-27 01:59:44 +0200

    feat(game): add traffic lights logic feat(light-duration): add util factory to handle light traffic durations

--- game-app/src/app/core/components/game/game.component.ts
+++ game-app/src/app/core/components/game/game.component.ts
@@ -1,32 +1,43 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { COMMOMS } from '../../const/common.const';
 import { NAVIGATION_PATH } from '../../const/navigation.const';
 import { UserData } from '../../models/entities/user/user-data-dto.model';
 import { UserService } from '../../services/user/user.service';
+import { LightDurationUtil } from '../../shared/utils/light-duration-util';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, AfterViewInit {
+  @ViewChild('light') light!: ElementRef<HTMLElement>;
   public actualPlayer: UserData = new UserData("");
   public NAVIGATION_PATH = NAVIGATION_PATH;
 
   private lastStep: boolean = true;
   private isFirstStep: boolean = true;
+  private lightCounter: number = COMMOMS.RED_DEFAULT_TIME;
   
   constructor(
     private readonly _route: ActivatedRoute,
     private readonly _userService: UserService
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
     this._route.paramMap.subscribe((params : ParamMap)=> {
       this.actualPlayer.name = params.get("name")!;      
       this.actualPlayer.score = Number(params.get("score")!);
       this.actualPlayer.maxScore = Number(params.get("maxScore")!);
-    })
+    })    
+  }
+
+  ngAfterViewInit(): void {
+    this.light.nativeElement.style.color = 'red';
+    setTimeout(() => {
+      this.setLight();
+    }, COMMOMS.RED_DEFAULT_TIME);    
   }
 
   @HostListener('window:beforeunload', ['$event'])
@@ -36,22 +47,41 @@ export class GameComponent implements OnInit {
   }
 
   public step(step: boolean) {
-    if (this.isFirstStep || this.lastStep !== step) {
+    //First step is used because player can start the game with left or right foot
+    if (this.light.nativeElement.style.color == 'green' && (this.isFirstStep || this.lastStep !== step)) {
       this.addPoint();
     } else {
-      this.removePoint();
+      this.light.nativeElement.style.color == 'red' ? this.actualPlayer.score = 0 : this.removePoint();
+      
     }
+    this.actualPlayer.maxScore = this.actualPlayer.score > this.actualPlayer.maxScore ? this.actualPlayer.score : this.actualPlayer.maxScore;
     this._userService.updateUser(this.actualPlayer);
     this.lastStep = step;
     this.isFirstStep = false;
   }
 
   private addPoint(): void {
-    this.actualPlayer.score = this.actualPlayer.score + 1;    
+    this.actualPlayer.score++;    
   }
 
   private removePoint(): void {
-    this.actualPlayer.score = this.actualPlayer.score - 1;    
+    //avoid negative score
+    if (this.actualPlayer.score > 0) {
+      this.actualPlayer.score--; 
+    }       
   }
 
+  private setLight(): void {
+    this.light.nativeElement.style.color = this.light.nativeElement.style.color == 'red' ? 'green' : 'red';
+    this.lightCounter = LightDurationUtil.getLightCounter(this.lightCounter, this.actualPlayer, this.light.nativeElement.style.color);
+    
+    //recursive
+    setTimeout(() => {
+      this.setLight();
+    }, this.lightCounter);
+  }
+
+  
+
 }
+
---
+++ game-app/src/app/core/shared/utils/light-duration-util.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { COMMOMS } from "../../const/common.const";
+import { UserData } from "../../models/entities/user/user-data-dto.model";
+
+@Injectable({
+	providedIn: 'root',
+})
+export class LightDurationUtil {
+
+    //Set counter for traffic light
+    static getLightCounter(lightCounter: number, player: UserData, currentColor : string): number {
+        return currentColor == 'red' ? COMMOMS.RED_DEFAULT_TIME : (player.score < 1 ? COMMOMS.GREEN_MAX_TIME : this.greenDuration(player, lightCounter));
+    }
+
+    //Only get the duration that can have the green traffic light
+    static greenDuration(player: UserData, lightCounter: number): number {
+        const duration = lightCounter <= COMMOMS.GREEN_MIN_TIME ? 
+        COMMOMS.GREEN_MIN_TIME : 
+        Math.max(COMMOMS.GREEN_MAX_TIME - player.score * 100, COMMOMS.GREEN_MIN_TIME) + this.getCustomVariation();   
+
+        //check duration again cause customVariation can return less time than GREEN_MIN_TIME
+        return duration >= COMMOMS.GREEN_MIN_TIME ? duration : COMMOMS.GREEN_MIN_TIME;
+    }
+
+    static getCustomVariation(): number {
+        return (Math.random() * (-COMMOMS.RAND_VARIATION_TIME - COMMOMS.RAND_VARIATION_TIME + 1) - COMMOMS.RAND_VARIATION_TIME)
+    }
+}
