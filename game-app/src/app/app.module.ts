1334aad09363fc35be46e67b5f5ca7bfb8f353f9
gzitto <gzitto@easyvirtualfair.com>
2021-10-27 11:22:43 +0200

    feat(project): add PWA to angular project

--- game-app/src/app/app.module.ts
+++ game-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { GameComponent } from './core/components/game/game.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,12 @@ import {MatIconModule} from '@angular/material/icon';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatIconModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
